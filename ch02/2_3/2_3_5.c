e8d40d8818190a5993506a32ed27b4647b734315
yergen sam <18231081626@163.com>
2016-11-09 16:24:37 +0800

    2_3_5.c

---
+++ ch02/2_3/2_3_5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int BinarySearch(int *A,int v);
+
+int BinarySearch(int *A,int length,int v)
+{
+	int low = 0;
+	int high = length;
+	int mid;
+
+	while(low < high)
+	{
+		mid = (low + high)/2;
+
+		if(A[mid] == v)
+			return mid;
+		else if(A[mid] < v)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	return -1;
+}
+
+int main()
+{
+	int A[6] = {1,3,4,6,7,8};
+	int v;
+	int index;
+	printf("Please input a number that below 9:");
+	scanf("%d",&v);
+	index = BinarySearch(A,6,v);
+	printf("The index is %d\n",index);
+
+	return 0;
+}
